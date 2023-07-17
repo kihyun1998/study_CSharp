d4264092ace9d9295f19f053aba6a1a545eeb9f1
pentakihyun <71832733+kihyun1998@users.noreply.github.com>
2023-07-17 21:41:22 +0900

    03

--- 03_Method/03_Method/Form1.Designer.cs
+++ 03_Method/03_Method/Form1.Designer.cs
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tBoxResult = new TextBox();
+            tBoxNum1 = new TextBox();
+            tBoxNum2 = new TextBox();
+            btnSum = new Button();
+            btnSub = new Button();
+            btnDiv = new Button();
+            btnMul = new Button();
+            SuspendLayout();
+            // 
+            // tBoxResult
+            // 
+            tBoxResult.Enabled = false;
+            tBoxResult.Location = new Point(21, 24);
+            tBoxResult.Name = "tBoxResult";
+            tBoxResult.Size = new Size(561, 39);
+            tBoxResult.TabIndex = 0;
+            tBoxResult.Text = "0";
+            tBoxResult.TextAlign = HorizontalAlignment.Right;
+            tBoxResult.TextChanged += textBox1_TextChanged;
+            // 
+            // tBoxNum1
+            // 
+            tBoxNum1.Location = new Point(21, 103);
+            tBoxNum1.Name = "tBoxNum1";
+            tBoxNum1.Size = new Size(274, 39);
+            tBoxNum1.TabIndex = 1;
+            tBoxNum1.Text = "0";
+            tBoxNum1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // tBoxNum2
+            // 
+            tBoxNum2.Location = new Point(308, 103);
+            tBoxNum2.Name = "tBoxNum2";
+            tBoxNum2.Size = new Size(274, 39);
+            tBoxNum2.TabIndex = 2;
+            tBoxNum2.Text = "0";
+            tBoxNum2.TextAlign = HorizontalAlignment.Right;
+            // 
+            // btnSum
+            // 
+            btnSum.Location = new Point(21, 191);
+            btnSum.Name = "btnSum";
+            btnSum.Size = new Size(97, 73);
+            btnSum.TabIndex = 3;
+            btnSum.Text = "+";
+            btnSum.UseVisualStyleBackColor = true;
+            btnSum.Click += btnSum_Click;
+            // 
+            // btnSub
+            // 
+            btnSub.Location = new Point(165, 191);
+            btnSub.Name = "btnSub";
+            btnSub.Size = new Size(97, 73);
+            btnSub.TabIndex = 4;
+            btnSub.Text = "-";
+            btnSub.UseVisualStyleBackColor = true;
+            btnSub.Click += btnSub_Click;
+            // 
+            // btnDiv
+            // 
+            btnDiv.Location = new Point(337, 191);
+            btnDiv.Name = "btnDiv";
+            btnDiv.Size = new Size(97, 73);
+            btnDiv.TabIndex = 5;
+            btnDiv.Text = "/";
+            btnDiv.UseVisualStyleBackColor = true;
+            btnDiv.Click += btnDiv_Click;
+            // 
+            // btnMul
+            // 
+            btnMul.Location = new Point(485, 191);
+            btnMul.Name = "btnMul";
+            btnMul.Size = new Size(97, 73);
+            btnMul.TabIndex = 6;
+            btnMul.Text = "*";
+            btnMul.UseVisualStyleBackColor = true;
+            btnMul.Click += btnMul_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(598, 384);
+            Controls.Add(btnMul);
+            Controls.Add(btnDiv);
+            Controls.Add(btnSub);
+            Controls.Add(btnSum);
+            Controls.Add(tBoxNum2);
+            Controls.Add(tBoxNum1);
+            Controls.Add(tBoxResult);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox tBoxResult;
+        private TextBox tBoxNum1;
+        private TextBox tBoxNum2;
+        private Button btnSum;
+        private Button btnSub;
+        private Button btnDiv;
+        private Button btnMul;
     }
 }
--- 03_Method/03_Method/Form1.cs
+++ 03_Method/03_Method/Form1.cs
@@ -6,5 +6,72 @@ namespace _03_Method
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(tBoxNum1.Text);
+            int num2 = int.Parse(tBoxNum2.Text);
+
+            tBoxResult.Text = fSum(num1,num2).ToString();
+            
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(tBoxNum1.Text);
+            int num2 = int.Parse(tBoxNum2.Text);
+
+            tBoxResult.Text = fSub(num1,num2).ToString();
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(tBoxNum1.Text);
+            int num2 = int.Parse(tBoxNum2.Text);
+
+            tBoxResult.Text = fDiv(num1,num2).ToString();
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(tBoxNum1.Text);
+            int num2 = int.Parse(tBoxNum2.Text);
+
+            int rst = num1 * num2;
+            tBoxResult.Text =fMul(num1,num2).ToString();
+        }
+
+        private int fSum(int num1, int num2)
+        {
+            int rst = 0;
+            rst = num1 + num2;
+            return rst;
+        }
+
+        private int fSub(int num1, int num2)
+        {
+            int rst = 0;
+            rst = num1 - num2; 
+            return rst;
+        }
+
+        private int fDiv(int num1, int num2)
+        {
+            int rst = 0;
+            rst = num1 / num2; 
+            return rst;
+        }
+        
+        private int fMul(int num1,int num2)
+        {
+            int rst = 0;
+            rst = num1 * num2;
+            return rst;
+        }
     }
 }
