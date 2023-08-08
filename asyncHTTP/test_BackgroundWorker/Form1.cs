a001718ba97e63777fb9e18a6c9e84ba39467330
penta-kihyun <71832733+kihyun1998@users.noreply.github.com>
2023-08-08 09:09:33 +0900

    비동기 요청 성공

---
+++ asyncHTTP/test_BackgroundWorker/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace test_BackgroundWorker
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.naver.com";
+            string resText = await RequestAsnc(url);
+            textBox1.Text = resText;
+        }
+
+        private async Task<string> RequestAsnc(string url)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "GET";
+
+            using (WebResponse res = await req.GetResponseAsync())
+            {
+                using (StreamReader reader = new StreamReader(res.GetResponseStream()))
+                {
+                    string resText = await reader.ReadToEndAsync();
+                    return resText;
+                }
+            }
+        }
+    }
+}
