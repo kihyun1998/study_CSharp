ba89f52c5835a51619237f25a9231596c7ba721c
penta-kihyun <71832733+kihyun1998@users.noreply.github.com>
2023-08-10 09:40:43 +0900

    파일 저장 및 폴더 생성 실습

---
+++ FileSaveLoad/FileSaveLoad/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileSaveLoad
+{
+    public partial class Form1 : Form
+    {
+        string checkPath = Path.GetFullPath(@"..\save");
+        string _filePath = Path.GetFullPath(@"..\save\test.txt");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // 폴더 유무 체크 및 폴더 생성
+            DirectoryInfo di = new DirectoryInfo(checkPath);
+            if (!di.Exists)
+            {
+                di.Create();
+            }
+            string txt = tBoxSave.Text;
+            System.IO.File.WriteAllText(_filePath, txt);
+        }
+    }
+}
