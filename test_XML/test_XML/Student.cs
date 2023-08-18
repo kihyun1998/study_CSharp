45e657194392fb54273bb932023944e24c1e46ba
penta-kihyun <71832733+kihyun1998@users.noreply.github.com>
2023-08-18 10:48:12 +0900

    study-xml

---
+++ test_XML/test_XML/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_XML
+{
+    class Student
+    {
+        public string STUDENT_NAME { get; set; }
+        public string STUDENT_GRADE { get; set; }
+    }
+}
