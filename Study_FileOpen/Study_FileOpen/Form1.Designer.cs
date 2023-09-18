4b206c171aba0276c67efdd45cec32a6a27ff59c
penta-kihyun <kihyun1998@naver.com>
2023-09-18 13:18:45 +0900

    Study : OpenFileDialog

---
+++ Study_FileOpen/Study_FileOpen/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Study_FileOpen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.tBoxRst = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(112, 334);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(585, 73);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tBoxRst
+            // 
+            this.tBoxRst.Location = new System.Drawing.Point(112, 38);
+            this.tBoxRst.Multiline = true;
+            this.tBoxRst.Name = "tBoxRst";
+            this.tBoxRst.Size = new System.Drawing.Size(585, 263);
+            this.tBoxRst.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tBoxRst);
+            this.Controls.Add(this.btnRun);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox tBoxRst;
+    }
+}
+
---
+++ Study_FileOpen/Study_FileOpen/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Study_FileOpen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileContent = string.Empty;
+            string filePath = string.Empty;
+
+            using(OpenFileDialog fd = new OpenFileDialog())
+            {
+                fd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                fd.Filter = "Text Files (*.txt)|*.txt|All files (*.*)|*.*";
+                fd.FilterIndex = 1;
+
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = fd.FileName;
+                }
+            }
+
+            FileStream fileStream = new FileStream(filePath, FileMode.Open);
+            using(StreamReader reader = new StreamReader(fileStream))
+            {
+                fileContent = reader.ReadToEnd();
+            }
+
+            tBoxRst.Clear();
+            tBoxRst.AppendText(filePath+"\r\n");
+            tBoxRst.AppendText(fileContent);
+        }
+    }
+}
