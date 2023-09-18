575dde3b311ef8943c419d76a4a4c84b0f87ebef
penta-kihyun <kihyun1998@naver.com>
2023-09-18 11:12:14 +0900

    c# winform으로 cmd 활용

---
+++ cmdStart/cmdStart/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace cmdStart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBoxCommand = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.tBoxRst = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tBoxCommand
+            // 
+            this.tBoxCommand.Location = new System.Drawing.Point(31, 32);
+            this.tBoxCommand.Name = "tBoxCommand";
+            this.tBoxCommand.Size = new System.Drawing.Size(490, 25);
+            this.tBoxCommand.TabIndex = 0;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(559, 32);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(88, 56);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // tBoxRst
+            // 
+            this.tBoxRst.Location = new System.Drawing.Point(31, 94);
+            this.tBoxRst.Multiline = true;
+            this.tBoxRst.Name = "tBoxRst";
+            this.tBoxRst.Size = new System.Drawing.Size(616, 443);
+            this.tBoxRst.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 566);
+            this.Controls.Add(this.tBoxRst);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.tBoxCommand);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBoxCommand;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox tBoxRst;
+    }
+}
+
---
+++ cmdStart/cmdStart/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cmdStart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo cmd = new ProcessStartInfo();
+            Process process = new Process();
+            cmd.FileName = @"cmd";
+            cmd.WindowStyle = ProcessWindowStyle.Hidden;
+            cmd.CreateNoWindow = true; // 새 창 띄우지 않음
+
+            cmd.UseShellExecute = false; // 운영체제 쉘 사용안함
+            
+            /*
+            StandardOutput : 애플리케이션의 텍스트 출력 스트림
+            StandardInput : 애플리케이션의 입력 스트림
+            StandardError : 애플리케이션의 오류 출력 스트림
+            */
+            
+            cmd.RedirectStandardOutput = true; // 출력을 StandardOutput 스트림에 사용
+            cmd.RedirectStandardInput = true; // 입력을 StandardInput 스트림에서 읽음
+            cmd.RedirectStandardError = true; // 오류 출력을 StandardError 스트림에서 사용
+
+            process.EnableRaisingEvents = false; // 프로세스 종료 시 Exited 이벤트 발생 안함
+            process.StartInfo = cmd;
+            process.Start();
+            process.StandardInput.Write(this.tBoxCommand.Text + Environment.NewLine); //Environment.NewLine = 줄바꿈 > \r\n으로 대체 가능
+            process.StandardInput.Close();
+
+            string result = process.StandardOutput.ReadToEnd();
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[Result Info]" + DateTime.Now + "\r\n");
+            sb.Append(result);
+            sb.Append("\r\n");
+
+            tBoxRst.Text = sb.ToString();
+            process.WaitForExit();
+            process.Close();
+
+            
+        }
+
+    }
+}
