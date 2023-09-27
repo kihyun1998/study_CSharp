58f2a484f92e51f3b0334ec42f4602a8d1d9fcad
penta-kihyun <kihyun1998@naver.com>
2023-09-27 10:17:39 +0900

    study create folder in specific path

---
+++ study-folder/study-folder/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace study_folder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            string folderName = ".apitest";
+            string folderPath = string.Format("{0}\\..\\{1}",Environment.GetFolderPath(Environment.SpecialFolder.Desktop),folderName);
+            DirectoryInfo di = new DirectoryInfo(folderPath);
+            if (!di.Exists)
+            {
+                di.Create();
+            }
+        }
+    }
+}
