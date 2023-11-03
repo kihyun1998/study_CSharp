cb1d094b080a20441bffdcd984d70c09d5bfdf3b
wantSome <kihyun1998@naver.com>
2023-11-03 11:18:21 +0900

    폴더뷰 시작

---
+++ folderview/folderview/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace folderview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.tBoxPath = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(32, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(214, 396);
+            this.treeView1.TabIndex = 0;
+            // 
+            // tBoxPath
+            // 
+            this.tBoxPath.Location = new System.Drawing.Point(389, 27);
+            this.tBoxPath.Name = "tBoxPath";
+            this.tBoxPath.Size = new System.Drawing.Size(352, 28);
+            this.tBoxPath.TabIndex = 1;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(561, 322);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(166, 86);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "button1";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.tBoxPath);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox tBoxPath;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ folderview/folderview/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace folderview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] msg = tBoxPath.Text.Split(' ');
+            for(var i = 0; i < msg.Length; i++)
+            {
+                MessageBox.Show(msg[i].ToString());
+            }
+        }
+    }
+}
